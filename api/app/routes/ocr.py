50ac064792c22df91a7dcbbbadb0cccca88ac5da
roggerfq <36053387+roggerfq@users.noreply.github.com>
2023-03-15 21:40:29 -0500

    Using the model for detecting rotated text

--- api/app/routes/ocr.py
+++ api/app/routes/ocr.py
@@ -7,21 +7,26 @@ from typing import List
 
 from fastapi import APIRouter, File, UploadFile, status
 
-from app.schemas import OCROut
+#from app.schemas import OCROut
+from app.schemas import OCRPolyOut
 from app.vision import predictor
 from doctr.io import decode_img_as_tensor
 
 router = APIRouter()
 
 
-@router.post("/", response_model=List[OCROut], status_code=status.HTTP_200_OK, summary="Perform OCR")
+#@router.post("/", response_model=List[OCROut], status_code=status.HTTP_200_OK, summary="Perform OCR")
+@router.post("/", response_model=List[OCRPolyOut], status_code=status.HTTP_200_OK, summary="Perform OCR")
 async def perform_ocr(file: UploadFile = File(...)):
     """Runs docTR OCR model to analyze the input image"""
     img = decode_img_as_tensor(file.file.read())
+    img = img.numpy()
     out = predictor([img])
-
+    
     return [
-        OCROut(box=(*word.geometry[0], *word.geometry[1]), value=word.value, confidence=word.confidence)
+        #OCROut(box=(*word.geometry[0], *word.geometry[1]), value=word.value)
+        #OCROut(box=(*word.geometry[0], *word.geometry[1]), value=word.value, confidence=word.confidence)
+        OCRPolyOut(polygon=word.geometry, value=word.value, confidence=word.confidence)
         for block in out.pages[0].blocks
         for line in block.lines
         for word in line.words
